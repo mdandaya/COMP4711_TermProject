b7a3f8c95c8c7000c42dfc59092a9ff8fd77f8a2
Jae Hyunhyo <jaehh1987@gmail.com>
2020-04-08 23:36:05 -0700

    commit for session variable test

--- controllers/msg-controller.js
+++ controllers/msg-controller.js
@@ -10,26 +10,31 @@ exports.msgSend = async function(req,res,next) {
     
     let subject = req.body.subject;
     let content = req.body.content;
-    let user1 = req.body.senderID;
-    let user2 = req.body.receiverID;
+    let user1 = req.session.userid;
+    let user2 = req.params.userid;
     
     // check the existance of the conversation
     var check;
     try {
         check = await model.checkConversation(user1, user2, subject);
         if (check.rowCount == 0) {
-            model.createConversation(user1, user2, subject, content);
-        } else {
-            model.createMessage(check.rows[0], content);
+            await model.createConversation(user1, user2, subject)
+            check = await model.checkConversation(user1, user2, subject);
         }
+        await model.createMessage(check.rows[0].id, content);
     } 
     catch (error) {
         console.log(error);
     }
+
+    // send user back to the counter-party's profile page
+    res.render('profile', { profileCSS: true });
 }
 
 exports.convList = function(req,res,next) {
-    res.render('msgList', { msgCSS: true });
+    console.log(req.session);
+    var conversations = model.conversationList(req.session.userID);
+    res.render('msgList', { msgCSS: true, conversations: conversations.rows });
 }
 
 exports.msgList = function(req,res,next) {
--- models/msg-model.js
+++ models/msg-model.js
@@ -5,11 +5,9 @@ function checkConversation(user1, user2, subject) {
     return db.query(sql, [user1, user2, subject]);
 }
 
-async function createConversation(user1, user2, subject, message) {
+function createConversation(user1, user2, subject, message) {
     let sql = 'INSERT INTO conversation (user1, user2, subject) VALUES ($1, $2, $3)';
-    await db.query(sql, [user1, user2, subject]);
-    let id = checkConversation(user1, user2, subject);
-    createMessage(id, message); 
+    db.query(sql, [user1, user2, subject]);
 }
 
 function createMessage(id, content) {
@@ -17,16 +15,25 @@ function createMessage(id, content) {
     db.query(sql, [id, content]);
 }
 
-function conversationList() {
-   return db.query('Select * from artists');
+function conversationList(myID) {
+    let sql = 'SELECT c.subject, u1.firstname as u1first, u1.lastname as u1last, \
+    u2.firstname as u2first, u2.lastname as u2last \
+    FROM conversation c \
+    JOIN users u1 ON c.user1 = u1.id \
+    JOIN users u2 ON c.user2 = u2.id \
+    WHERE user1 = $1 OR user2 = $1';
+    return db.query(sql, myID);
 }
 
-function messageList() {
-    return db.query('Select * from artists');
+function messageList(convID) {
+    let sql = 'SELECT * FROM message WHERE convid = $1';
+    return db.query(sql, convID);
 }
 
 module.exports = {
    checkConversation : checkConversation,
    createConversation : createConversation,
-   createMessage : createMessage
+   createMessage : createMessage,
+   conversationList : conversationList,
+   messageList : messageList
 }
